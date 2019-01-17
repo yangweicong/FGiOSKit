59679187b7a58efe1bffe0c7a1265ee6cc344f7f
figo_ywc <yangwc@figo.cn>
2019-01-17 17:58:55 +0800

    no message

--- FGiOSKitDemo/Pods/Target Support Files/Pods-FGiOSKitDemo/Pods-FGiOSKitDemo-resources.sh
+++ FGiOSKitDemo/Pods/Target Support Files/Pods-FGiOSKitDemo/Pods-FGiOSKitDemo-resources.sh
@@ -97,11 +97,9 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "${PODS_ROOT}/EasyNavigation/EasyNavigation/EasyNavButton.bundle"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "${PODS_ROOT}/EasyNavigation/EasyNavigation/EasyNavButton.bundle"
   install_resource "${PODS_ROOT}/MJRefresh/MJRefresh/MJRefresh.bundle"
 fi
 
