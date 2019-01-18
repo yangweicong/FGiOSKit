e1d21379095f0362fedeb1e56b49485cd487a8a9
figo_ywc <yangwc@figo.cn>
2019-01-18 17:48:25 +0800

    添加 address 类

---
+++ FGiOSKit/iOSKit/Component/Address/FGAddressModel.m
@@ -0,0 +1,13 @@
+//
+//  FGAddressModel.m
+//  yulala
+//
+//  Created by mac on 2019/1/18.
+//  Copyright © 2019年 YangWeiCong. All rights reserved.
+//
+
+#import "FGAddressModel.h"
+
+@implementation FGAddressModel
+
+@end
---
+++ FGiOSKit/iOSKit/Component/Address/FGOderAddressDisplayView.h
@@ -0,0 +1,17 @@
+//
+//  FGOderAddressDisplayView.h
+//  yulala
+//
+//  Created by mac on 2019/1/18.
+//  Copyright © 2019年 YangWeiCong. All rights reserved.
+//
+
+#import "FGBaseView.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FGOderAddressDisplayView : FGBaseView
+
+@end
+
+NS_ASSUME_NONNULL_END
